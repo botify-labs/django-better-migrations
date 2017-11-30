2d014297b7d5225b30e75ef4e20193d16b5cf412
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2017-11-30 17:47:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Django Better Migrations
+========================
+
+This project aims at providing improvements to Django's default migration system.
+The default migration system is over-engineered, sometimes dangerous, and not
+team work friendly.
+
+TODO: document more the rationale and why not direct contributions to Django upstream.
+
+
+Features
+--------
+
+- [ ] directly see generated SQL in migration files
+- [ ] implement a set of rules to protect against dangerous actions in production
+- [ ] support team-friendly migrations, with no dependencies management and unique names
+
+
+Installation
+------------
+
+TODO
+
+
+License
+-------
+
+MIT, see `LICENSE` file.
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+version = __import__("django-better-migrations").__version__
+
+
+def read(path):
+    """Return the content of a file."""
+    with open(path, "r") as f:
+        return f.read()
+
+
+setup(
+    name="django-better-migrations",
+    version=version,
+    description="Improves Django migration system.",
+    long_description=(read("README.md")),
+    url="http://github.com/botify-labs/django-better-migrations",
+    license="MIT",
+    author="Jean-Baptiste Barth",
+    author_email="jeanbaptiste.barth@gmail.com",
+    packages=find_packages(exclude=["tests*"]),
+    scripts=[],
+    install_requires=[],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Database",
+    ],
+)
