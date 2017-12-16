a4db9fe839c1dd6d370de10b9dbd004c86bab655
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2017-12-17 00:09:02 +0100

    Disable sure's dir patch, useless in our case and breaks on OSX at least
    Full trace:
    
        ___________________________________________________________ TestMakemigrationsAddsSql.test_makemigrations_adds_a_migration_file ____________________________________________________________
        .tox/py27-django19/lib/python2.7/site-packages/freezegun/api.py:494: in wrapper
            with self:
        .tox/py27-django19/lib/python2.7/site-packages/freezegun/api.py:365: in __enter__
            return self.start()
        .tox/py27-django19/lib/python2.7/site-packages/freezegun/api.py:421: in start
            for module_attribute in dir(module):
        .tox/py27-django19/lib/python2.7/site-packages/sure/__init__.py:1081: in _new_dir
            patched = [x for x in old_dir(obj[0]) if isinstance(getattr(obj[0], x, None), AssertionBuilder)]
        .tox/py27-django19/lib/python2.7/site-packages/pkg_resources/_vendor/six.py:92: in __get__
            result = self._resolve()
        .tox/py27-django19/lib/python2.7/site-packages/pkg_resources/_vendor/six.py:115: in _resolve
            return _import_module(self.mod)
        _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
    
        name = 'gdbm'
    
            def _import_module(name):
                """Import module, returning the module after the last dot."""
        >       __import__(name)
        E       ImportError: No module named gdbm
    
        .tox/py27-django19/lib/python2.7/site-packages/pkg_resources/_vendor/six.py:82: ImportError

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,8 @@
 import sure
 import tests.assertions
+
+# Disable sure's patched "dir" builtin, since it breaks on OSX
+# with an error similar to: https://github.com/benjaminp/six/issues/54
+# (having sure AND six AND tox trying to monkey-patch things might be too much)
+from sure import builtins, old_dir
+builtins.dir = old_dir
